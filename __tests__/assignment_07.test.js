436b3bfc5562593a83e889d75a5f05b0fc03eb56
Abebe Bediye <51907363+abebediye@users.noreply.github.com>
2024-02-14 12:06:50 +0100

    Fikse testen for oppgave 07
    endre expected verdi for encrypt melding 
    fjerne .skip i decrypt test
    legge til key parameter = 3 i decrypt test

--- __tests__/assignment_07.test.js
+++ __tests__/assignment_07.test.js
@@ -1,29 +1,29 @@
 import { expect } from "@jest/globals";
 import { decrypt, encrypt } from "../assignments/assignment_07";
-
+ 
 describe("Oppgave 07", () => {
     it("Krypter melding med Cease cypher", () => {
         // arrange
         const message = "Experience is the teacher of all things.";
-
-        const expected = "Lbwlyplujl pz æol ælhjoly vm hss æopunz.";
-
+ 
+        const expected = "Lewlyplujl pz aol alhjoly vm hss aopunz.";
+ 
         // act
         const result = encrypt(message, 7);
-
+ 
         // assert
         expect(result).toEqual(expected);
     });
-
+ 
     it("Dekrypter melding med Cease cypher", () => {
         // arrange
         const message = "Mhj hu hq Dzhvrph surjudphuhu!";
-
+ 
         const expected = "Jeg er en Awesome programerer!";
-
+ 
         // act
         const result = decrypt(message, 3);
-
+ 
         // assert
         expect(result).toEqual(expected);
     });
